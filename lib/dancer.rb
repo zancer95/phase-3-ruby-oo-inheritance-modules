8736c92c77e8e655fe3aa94e5fa11de397098ed6
ZhiLeong <saber_zhileong@hotmail.com>
2023-01-16 14:06:06 -0500

    commit tree 727c94470d1f7dde56e85b025eee387232d9d468

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance.rb'
+
+class Dancer
+    include Dance
+    extend MetaDancing
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+  end
